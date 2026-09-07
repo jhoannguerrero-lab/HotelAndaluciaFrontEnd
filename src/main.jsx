e69e142e1fc050fe99593793583a5ac869bd4f7b
Jhoann Guerrero <jhoann.guerrero@ucb.edu.bo>
2026-09-06 22:19:36 -0400

    Se agrega estilos base con la paleta de Andalucia

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import 'normalize.css'
+import './styles/variables.css'
+import './styles/base.css'
+import './styles/layout.css'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
